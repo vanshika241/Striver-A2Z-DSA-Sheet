e89fa2028db2a687272a80318efa2d98e7888236
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-12-25 00:16:37 +0530

    Create BS-1D.java

---
+++ Step-04/BinarySearch/BS-1D.java
@@ -0,0 +1,218 @@
+// Binary Search
+public int search(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                start =  mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+
+//Order Agnostic Binary Search 
+public int search(int[] nums, int target) {
+        
+        int n = nums.length;
+        boolean isAsc = true;
+        if(nums[0]>nums[n-1]){
+          isAsc = false;
+        }
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+              if(Asc){
+                if(nums[mid] < target){
+                start =  mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+              }
+          else{
+            if(nums[mid] < target){
+                end =  mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+             
+        }
+        return -1;
+    }
+
+
+// Implement Lower bound 
+int lowerBound(int[] arr, int target) {
+        // code here
+        int n = arr.length;
+        int ans = n;
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+             if(arr[mid] >= target){
+                ans = mid;
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+
+//Implement Upper bound
+int upperBound(int[] arr, int target) {
+        // code here
+        int n = arr.length;
+        int ans = n;
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+             if(arr[mid] >target){
+                ans = mid;
+                end = mid-1;
+            }
+            else{
+                
+                start = mid+1;
+            }
+        }
+        return ans;
+
+    }
+
+  //Find floor/Ceil in a sorted array 
+  public int findFloor(int[] arr, int target) {
+        // code here
+        int n = arr.length;
+        int ans = -1;
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+             if(arr[mid] <=target){
+                ans = mid;
+                start = mid+1;
+            }
+            else{
+                
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+
+  public int findCeil(int[] arr, int target) {
+        // code here
+        // code here
+        int n = arr.length;
+        int ans = -1;
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+             if(arr[mid] >=target){
+                ans = mid;
+                end = mid-1;
+            }
+            else{
+                
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+//Search in Insert position 
+public int searchInsert(int[] nums, int target) {
+         int n = nums.length;
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                start =  mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return start;
+    }
+
+//Find the first or last occurrence of a given number in a sorted array
+    public int findFirstOcc(int nums[] , int target){
+         int n = nums.length;
+        int start = 0;
+        int end = n-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                ans=  mid;
+                end = mid-1;
+            }
+            else if(nums[mid] < target){
+                start =  mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+
+    public int findLastOcc(int nums[] , int target){
+         int n = nums.length;
+        int start = 0;
+        int end = n-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                ans=  mid;
+                start = mid+1;
+            }
+            else if(nums[mid] < target){
+                start =  mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirstOcc(nums , target);
+        int last = findLastOcc(nums , target);
+        if(first == -1)return new int[]{-1,-1};
+        return new int[]{first , last};
+    }
+
+//Count occurrences of a number in a sorted array with duplicates
+int countFreq(int[] arr, int target) {
+        // code here
+        
+        int first = findFirstOcc(arr , target);
+        int last = findLastOcc(arr , target);
+        if(first == -1)return 0;
+        return last-first+1;
+        
+    }
