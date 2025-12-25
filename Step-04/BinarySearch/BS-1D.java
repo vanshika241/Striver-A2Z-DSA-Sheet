d3fe81c21a27c7816ea42b1bdc7104cf97477d0f
codeVibes:) <123478862+vanshika241@users.noreply.github.com>
2025-12-25 13:37:42 +0530

    Update BS-1D.java

--- Step-04/BinarySearch/BS-1D.java
+++ Step-04/BinarySearch/BS-1D.java
@@ -216,3 +216,92 @@ int countFreq(int[] arr, int target) {
         return last-first+1;
         
     }
+
+//Search in Rotated Sorted Array I
+public int search(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n-1;
+        while(start <=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[start]<=nums[mid]){
+                if(nums[start]<=target && nums[mid]>target){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && nums[end]>=target){
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+
+    }
+
+//Search in Rotated Sorted Array II
+public boolean search(int[] nums, int target) {
+         int n = nums.length;
+        int start = 0;
+        int end = n-1;
+        while(start <=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                return true;
+            }
+            if(nums[start] == nums[mid] && nums[mid] == nums[end]){
+                start++;
+                end--;
+            }
+            else if (nums[start]<=nums[mid]){
+                if(nums[start]<=target && nums[mid]>target){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && nums[end]>=target){
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
+        }
+        return false;
+
+    }
+
+//Find Minimum in Rotated Sorted Array
+ public int findMin(int[] nums) {
+        int n = nums.length;
+        int start = 0;
+        int ans = Integer.MAX_VALUE;
+        int end = n-1;
+        if(nums[0]<=nums[end]){
+            return nums[0];
+        }
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[start]<=nums[mid]){
+                ans = Math.min(ans , nums[start]);
+                start = mid+1;
+            }
+            else{
+                 ans = Math.min(ans , nums[mid]);
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
